5584a53199dbbdd2a75c6da57a6e4f56a0d51c8e
bernardofv99 <bernardo.flores@energyatech.com>
2022-04-17 23:56:36 -0500

    update

--- week04/double.h
+++ week04/double.h
@@ -20,6 +20,210 @@ class DoubleList : public List<T>{
     public:
         DoubleList() : List() { }
 
+        void push_front(T value) override {
+            auto new_node = new Node<T>(value);
+
+            if(this->empty()){
+                this->head = new_node;
+                this->tail = new_node;
+            }else{
+                auto tmp = this->head;
+                this->head = new_node;
+                tmp->prev = this->head;
+                this->head->next = tmp;
+            }
+            this->nodes++;
+        }
+
+        void push_back(T value)override {
+            auto new_node = new Node<T>(value);
+
+            if(this->empty()){
+                this->head = new_node;
+                this->tail = new_node;
+            }else{
+                this->tail->next = new_node;
+                new_node->prev = this->tail;
+                this->tail = new_node;
+            }
+
+            this->nodes++;
+        }
+
+        T pop_front()override {
+            if (this->empty()) {
+                return 0;
+            }
+
+            auto tmp = this->head;
+            auto ret = this->head->data;
+            this->head = this->head->next;
+            this->head->prev = nullptr;
+            delete tmp;
+            this->nodes--;
+            return ret;
+        }
+
+        T pop_back()override {
+            if (this->empty()) {
+                return 0;
+            }
+
+            auto tmp = this->tail;
+            auto ret = this->tail->data;
+            //this->tail = this->tail->prev;
+
+            this->tail = this->tail->prev;
+            this->tail = nullptr;
+
+            delete tmp;
+            this->nodes--;
+            return ret;
+        }
+
+        T insert(T val, int pos)override {
+            if(pos > this->size()){
+                return -1;
+            }
+            if(this->empty()){
+                return -1;
+            }
+
+            if(pos == 0){
+                this->push_front(val);
+                return val;
+            }
+
+            if(pos == this->size()-1){
+                this->push_back(val);
+                return val;
+            }
+
+            auto newnode = new Node<T>(val);
+
+            auto tmp_prev = this->head;
+
+            for(auto i = 0; i < pos - 1; i++){
+                tmp_prev = tmp_prev->next;
+            }
+            newnode->next = tmp_prev->next;
+            tmp_prev->next->prev = newnode;
+
+            tmp_prev->next = newnode;
+            newnode->prev = tmp_prev;
+
+            tmp_prev = nullptr;
+            delete tmp_prev;
+            this->nodes++;
+        }
+
+        void remove(int pos)override {
+            if(pos == 0){
+                this->pop_front();
+                return;
+            }
+
+            if(pos == this->size()-1){
+                this->pop_back();
+                return;
+            }
+
+            if(pos > this->size()){
+                return;
+            }
+            if(this->empty()){
+                return;
+            }
+
+            auto tmp_prev = this->head;
+
+            for(auto i = 0; i < pos - 1; i++){
+                tmp_prev = tmp_prev->next;
+            }
+
+            auto tmp_nxt = tmp_prev->next;
+            tmp_prev->next = tmp_nxt->next;
+            tmp_nxt->next->prev = tmp_prev;
+
+            delete tmp_prev, tmp_nxt;
+            this->nodes--;
+        }
+
+        T operator[](int pos){
+            if (is_empty()){
+                throw "List is empty";
+            }
+            if(pos >= this->nodes){
+                throw "Index no valido";
+            }
+            Node<T> *temp = this->head;
+            int i = 0;
+            while (i < pos){
+                temp = temp->next;
+                i++;
+            }
+            return temp->data;
+        };
+
+        bool is_sorted(){
+            int i = 0;
+            while(i< this->nodes - 1){
+                if(operator[](i) > operator[](i+1)){
+                    return false;
+                }
+                ++i;
+            }
+            return true;
+        };
+
+        bool is_empty(){
+            return this->head == nullptr;
+        };
+
+        int size() {
+            return this->nodes;
+        };
+
+        void clear() {
+            while(!this->empty()){
+                pop_front();
+            }
+        };
+
+        void reverse(){
+            Node<T> *nodo = this->head;
+            Node<T> *temp = this->head;
+            List<T> *list= new DoubleList<T>();
+            while(nodo != nullptr){
+                list->push_front(nodo->data);
+                nodo = nodo->next;
+            }
+            int i = 0;
+            while(temp != nullptr){
+                temp->data = list->operator[](i);
+                temp = temp->next;
+                i++;
+            }
+        };
+
+        std::string name(){
+            return "Double List";
+        };
+
+        void display(){
+            if (is_empty()){
+                throw "List is empty";
+            }
+            Node<T>* temp = this->head;
+            while(temp != nullptr){
+                cout << temp->data << " -> ";
+                temp = temp->next;
+            }
+            cout << endl;
+        };
+
+
+
         /* new functions */             
         iterator begin() {
             // TODO
--- week04/list.h
+++ week04/list.h
@@ -1,7 +1,10 @@
+using namespace std;
 #ifndef LIST_H
 #define LIST_H
 
 #include "node.h"
+#include <iostream>
+
 
 // TODO: Implement all methods
 template <typename T>
@@ -17,24 +20,192 @@ class List {
             // TODO            
         } ;
 
-        virtual T front() = 0;
-        virtual T back() = 0;
-        virtual void push_front(T) = 0;
-        virtual void push_back(T) = 0;
-        virtual T pop_front() = 0;
-        virtual T pop_back() = 0;
-        virtual T insert(T, int) = 0;
-        virtual bool remove(T) = 0;
-        virtual T operator[](int) = 0;
-        virtual bool empty() = 0;
+        virtual T front(){
+            return head->data;
+        };
+
+        virtual T back() {
+            return tail->data;
+        };
+
+        virtual void push_front(T value) override {
+            auto new_node = new Node<T>(value);
+
+            if(this->empty()){
+                this->head = new_node;
+                this->tail = new_node;
+            }else{
+                auto tmp = this->head;
+                this->head = new_node;
+                this->head->next = tmp;
+            }
+            this->nodes++;
+        }
+
+        virtual void push_back(T value) override {
+
+            auto new_node = new Node<T>(value);
+
+            if(this->empty()){
+                this->head = new_node;
+                this->tail = new_node;
+            }else{
+                this->tail->next = new_node;
+                this->tail = new_node;
+            }
+
+            this->nodes++;
+        }
+
+        virtual T pop_front() override {
+            if (this->empty()) {
+                return 0;
+            }
+
+            auto tmp = this->head;
+            auto ret = this->head->data;
+            this->head = this->head->next;
+            delete tmp;
+            this->nodes--;
+            return ret;
+        }
+
+        virtual T pop_back()override {
+            if (this->empty()) {
+                return 0;
+            }
+            auto tmphead = this->head;
+            auto tmp = this->tail;
+            auto ret = this->tail->data;
+            //this->tail = this->tail->prev;
+            while(tmphead->next != this->tail){
+                tmphead = tmphead->next;
+            }
+
+            this->tail = tmphead;
+            tmphead = nullptr;
+            delete tmp;
+            delete tmphead;
+            this->nodes--;
+            return ret;
+        }
+
+        virtual T insert(T val, int pos)override {
+            if(pos == 0){
+                this->push_front(val);
+                return val;
+            }
+
+            if(pos == this->size()-1){
+                this->push_back(val);
+                return val;
+            }
+
+            auto newnode = new Node<T>(val);
+
+            if(pos > this->size()){
+                return -1;
+            }
+            if(this->empty()){
+                return -1;
+            }
+
+            auto tmp_prev = this->head;
+            auto tmp_nxt = this->head->next;
+            for(auto i = 0; i < pos - 1; i++){
+                tmp_prev = tmp_nxt;
+                tmp_nxt = tmp_nxt->next;
+            }
+
+            tmp_prev->next = newnode;
+            newnode->next = tmp_nxt;
+
+            tmp_prev = nullptr;
+            delete tmp_prev, tmp_nxt;
+            this->nodes++;
+        }
+
+        virtual bool remove(int pos) {
+            if (is_empty()){
+                throw "List is empty";
+            }
+            if(pos >= this->nodes){
+                throw "Index no valido";
+            }
+            Node<T> *temp = this->head;
+            int i = 0;
+            while (i < pos-1 && temp != nullptr){
+                temp = temp->next;
+                i++;
+            }
+            Node<T> *temp1 = temp->next;
+            temp->next = temp1->next;
+            delete temp1;
+            this->nodes--;
+        };
+
+        virtual T operator[](int val)override {
+            if(val < this->nodes){
+                auto tmp = this->head;
+                for(int i = 0; i < val; i++){
+                    tmp = tmp->next;
+                }
+                return tmp->data;
+            }
+        }
+
+        virtual bool empty(){
+            return !nodes;
+        };
+
         virtual bool find(T) = 0;
-        virtual int size() = 0;
-        virtual void clear() = 0;
+
+        virtual int size(){
+            return nodes;
+        }
+
+        virtual void clear(){
+            while(!this->empty()){
+                pop_front();
+            }
+        }
         virtual void sort() = 0;
-        virtual bool is_sorted() = 0;
-        virtual List* reverse() = 0;
-        virtual void display(std::ostream& os) = 0;
-        virtual string name() = 0;
+
+        virtual bool is_sorted(){
+            auto tmp = this->head;
+            while(tmp->next != nullptr || tmp == this->tail){
+                if (tmp->data > tmp->next->data){
+                    return false;
+                }
+                tmp = tmp->next;
+            }
+            return true;
+        }
+
+        List<T>* reverse(){
+            List<T>* newlist = new ForwardList<T>();
+            auto tmp = this->head;
+            for(;tmp != nullptr;tmp=tmp->next){
+                newlist->push_front(tmp->data);
+            }
+            return newlist;
+        }
+
+        virtual void display(std::ostream& os) {
+            if (is_empty()){
+                throw "List is empty";
+            }
+            Node<T>* temp = this->head;
+            while(temp != nullptr){
+                cout << temp->data << " => ";
+                temp = temp->next;
+            }
+            cout << endl;
+        };
+
+        virtual std::string name(){
+            return "List";
+        };
 };
 
 #endif
